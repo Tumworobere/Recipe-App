eb28b6b27fc8c9e28bcc419df64431ca3ae9ac8a
Anselem <anselemodims@gmail.com>
2022-04-12 11:43:52 +0100

    Chore: fix linters

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -12,18 +12,18 @@ class FoodsController < ApplicationController
   def new
     @food = Food.new
   end
-  
+
   def create
     @food = current_user.foods.new(food_params)
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to foods_path, notice: 'Food was created successfully'}
+        format.html { redirect_to foods_path, notice: 'Food was created successfully' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
     end
-  end 
+  end
 
   def destroy
     @food = Food.find(params[:id])
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,12 +1,8 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-      user ||= User.new # guest user (not logged in)
-      if user.present?
-        can :manage, Food, user: user
-      end
+    user ||= User.new # guest user (not logged in)
+    can :manage, Food, user: user if user.present?
   end
 end
