034d1ea7cddfb827da7e25e37be7beaa0ac1aed5
Annah Tomoro <annaht.annah@gmail.com>
2022-04-12 18:21:01 +0300

    Add and show recipes

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,64 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: %i[ show edit update destroy ]
+
+  
+  def index
+    @recipes = current_user.recipes
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def edit
+  end
+
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully created." }
+        format.json { render :show, status: :created, location: @recipe }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @recipe.update(recipe_params)
+        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated." }
+        format.json { render :show, status: :ok, location: @recipe }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @recipe.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+
+    respond_to do |format|
+      format.html { redirect_to recipes_url, notice: "Recipe was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    
+    def recipe_params
+      params.require(:recipe).permit(:name, :preparationTime, :cookingTime, :description, :public)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -37,5 +37,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_12_075858) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.decimal "preparationTime"
+    t.decimal "cookingTime"
+    t.text "description"
+    t.boolean "public"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
   add_foreign_key "foods", "users"
 end
