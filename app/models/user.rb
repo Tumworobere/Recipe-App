e273897a6f62836344a09fd3bdaf96f84662589d
Annah Tomoro <annaht.annah@gmail.com>
2022-04-12 18:51:50 +0300

    Update Model users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :foods, foreign_key: :user_id, dependent: :destroy
+  has_many :recipes, dependent: :destroy
+
 end
