e9549750d0ac5ffc5d5b84e39ec8926dfd87f184
mong'are <mongaresams@gmail.com>
2022-04-13 07:37:21 +0300

    Add description field to inventory table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_13_033029) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_13_043610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_13_033029) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
