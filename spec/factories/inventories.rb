2bfd976f7a02b596f8ebd6233aaa2b6b6acd1c2f
mong'are <mongaresams@gmail.com>
2022-04-14 01:46:05 +0300

    Incorporate factory bot

---
+++ spec/factories/inventories.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :inventory do
+    
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,7 +41,11 @@ Capybara.javascript_driver = :selenium_chrome
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include FactoryBot::Syntax::Methods
 
+  config.include FactoryBot::Syntax::Methods
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
