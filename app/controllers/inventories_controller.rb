ad29c1cee84d2f20fc92ee22eaaffd03e8434061
mong'are <mongaresams@gmail.com>
2022-04-13 07:34:57 +0300

    Create new file to add inventories

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,6 +1,29 @@
 class InventoriesController < ApplicationController
+  load_and_authorize_resource
 
   def index
     @inventories = Inventory.all
+  end
+
+  def new
+    @inventory = Inventory.new
+  end
+
+  def create
+    @inventory = current_user.inventories.new(inventory_params)
+
+    respond_to do |format|
+      if @inventory.save
+        format.html { redirect_to inventories_path, notice: 'Inventory was created successfully' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:name, :description)
   end
 end
