d37d8e3ecfbc75685519c78d1335f7e28b34cb01
Annah Tomoro <annaht.annah@gmail.com>
2022-04-13 20:08:16 +0300

    Fix errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, Food, user: user if user.present?
+    can :manage, Recipe, user: user if user.present?
     can :manage, Inventory, user: user if user.present?
   end
 end
