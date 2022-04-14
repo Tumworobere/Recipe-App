f4e3f0050f182970f306b012c0944beda6d729c5
mong'are <mongaresams@gmail.com>
2022-04-14 11:55:10 +0300

    Remove recipies from the inventory routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,14 +17,10 @@ Rails.application.routes.draw do
   resources :users, only: [:index]
   resources :foods, except: [:update]
   resources :recipes,except: [:update]
-  resources :recipe_foods,except: [:update]
 
   # Defines the routes for the inventory and inventory_foods controller
   resources :inventories, except: %i[update] do
     resources :inventory_foods, only: %i[update]
-    resources :recipes,except: %i[update] do
-      resources :recipe_foods,only: %i[update]
-      
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_13_043610) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_13_132322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,28 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_13_043610) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_foods_on_user_id"
-  end
-
-  create_table "recipes", force: :cascade do |t|
-    t.string "name"
-    t.decimal "preparationTime"
-    t.decimal "cookingTime"
-    t.text "description"
-    t.boolean "public"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_recipes_on_user_id"
-  end
-  
-  create_table "recipe_foods", force: :cascade do |t|
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "recipe_id", null: false
-    t.bigint "food_id", null: false
-    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
-    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
   create_table "inventories", force: :cascade do |t|
@@ -61,8 +39,29 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_13_043610) do
     t.bigint "food_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user"
     t.index ["food_id"], name: "index_inventory_foods_on_food_id"
     t.index ["inventory_id"], name: "index_inventory_foods_on_inventory_id"
+  end
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "recipe_id", null: false
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.decimal "preparationTime"
+    t.decimal "cookingTime"
+    t.text "description"
+    t.boolean "public"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -79,10 +78,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_13_043610) do
   end
 
   add_foreign_key "foods", "users"
-  add_foreign_key "recipes", "users"
   add_foreign_key "inventories", "users"
   add_foreign_key "inventory_foods", "foods"
   add_foreign_key "inventory_foods", "inventories"
-  add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
+  add_foreign_key "recipes", "users"
 end
