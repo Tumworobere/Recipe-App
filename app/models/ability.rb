fd06056f03e2fb8b8fd78ba3188330c081ca40d1
mong'are <mongaresams@gmail.com>
2022-04-15 17:23:51 +0300

    Fix abilities issues

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,9 +5,9 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :manage, Food, user: user if user.present?
     can :manage, Inventory, user: user if user.present?
-    can :manage, InventoryFood, user: user if user.present?
+    can :manage, InventoryFood, inventory: { user_id: user.id }
     can :manage, Recipe, user: user if user.present?
     can :read, :all if user.present?
-    can :create, :all if user.present?
+
   end
 end
