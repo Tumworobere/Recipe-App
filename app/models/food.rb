6f9ec2a855d94e670a2fb9185665e79e988e08d0
Anselem <anselemodims@gmail.com>
2022-04-15 07:57:12 +0100

    Test: write tests for association of food to user and numericality of the fields

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,5 @@ class Food < ApplicationRecord
   has_many :inventory_foods, foreign_key: :food_id, dependent: :destroy
   has_many :recipe_foods, foreign_key: :food_id, dependent: :destroy
   validates :food, :measurement_unit, :unit_price, presence: true
+  validates :measurement_unit, :unit_price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
+  
+  context 'associations' do
+    it { should belong_to(:user).class_name('User') }
+  end
+
   context 'values not empty' do
     it { should validate_presence_of(:food) }
     it { should validate_presence_of(:measurement_unit) }
     it { should validate_presence_of(:unit_price) }
+  end
+
+  context 'values should be numerical' do
+    it { should validate_numericality_of(:measurement_unit) }
+    it { should validate_numericality_of(:unit_price) }
   end
 end
