d3b6b45621cf3bcac0d75ee214fe4c0f7e11bb77
Anselem <anselemodims@gmail.com>
2022-04-15 09:00:05 +0100

    Test: write tests for the InventoryFood model

---
+++ spec/models/inventory_foods_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe InventoryFood, type: :model do
+  context 'associations' do
+    it { should belong_to(:food).class_name('Food') }
+    it { should belong_to(:inventory).class_name('Inventory') }
+  end
+
+  context 'values not empty' do
+    it { should validate_presence_of(:quantity) }
+  end
+
+  context 'values should be numerical' do
+    it do
+      should validate_numericality_of(:quantity).
+        is_greater_than_or_equal_to(0)
+    end
+  end
+end
